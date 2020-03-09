2d718c32e435e13d2750240cbb1b013df58d777d
YasirHasn9 <hassanyasir223@gmail.com>
2020-03-08 17:55:06 -0700

    GET '/smurfs' is done

--- smurfs/src/actionCreator/index.js
+++ smurfs/src/actionCreator/index.js
@@ -7,16 +7,12 @@ export const FETCH_DATA_FAILURE = "FETCH_DATA_FAILURE";
 // action creator
 // get the data from the api
 
-export const getSmurfs = () => {
-  return dispatch => {
-    dispatch({ type: FETCH_DATA_SUCCESS });
-    axios
-      .get("http://localhost:3333/smurfs")
-      .then(res => {
-        dispatch({ type: FETCH_DATA_SUCCESS, payload: res.data });
-      })
-      .catch(err => {
-        dispatch({ type: FETCH_DATA_FAILURE, payload: err });
-      });
-  };
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: FETCH_DATA_START });
+  axios
+    .get("http://localhost:3333/smurfs")
+    .then(res => {
+      dispatch({ type: FETCH_DATA_SUCCESS, payload: res.data });
+    })
+    .catch(err => dispatch({ type: FETCH_DATA_FAILURE, payload: err }));
 };
--- smurfs/src/components/Smurfs.js
+++ smurfs/src/components/Smurfs.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
+import { getSmurfs } from "../actionCreator/index";
 
 function Smurfs(props) {
-  console.log(props);
-  
-  return (
-      <>
+  console.log("and this the props", props.smurfs);
 
-      sumrfs
-      </>
+  const fetchSmurf = e => {
+    e.preventDefault();
+    props.getSmurfs();
+  };
+
+  return (
+    <div>
+      {props.isLoading && <p>Fetching your Smurfs</p>}
+      <div>
+        {props.smurfs.map(smurf => (
+          <div>
+            <h4 key={smurf.id}>{smurf.name}</h4>
+            <p>
+              Hello, my name is {smurf.name}.<br />
+              My age is {smurf.age}.<br />
+              My height is {smurf.height}.{" "}
+            </p>
+          </div>
+        ))}
+      </div>
+      {props.error && <p>{props.error}</p>}
+      <button onClick={fetchSmurf}>Get Smurfs</button>
+    </div>
   );
 }
 
@@ -19,7 +38,34 @@ const mapStateToProps = store => {
   };
 };
 
+const mapDispatchToProps = {
+  getSmurfs
+};
+
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(Smurfs);
+
+/*
+
+
+
+  return 
+};
+
+function mapStateToProps(state) {
+  return {
+    smurfs: state.smurfs,
+    error: state.error,
+    isFetching: state.isFetching
+  };
+};
+
+const mapDispatchToProps = {
+    getSmurfs,
+    addSmurf
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Smurfs);
+*/
